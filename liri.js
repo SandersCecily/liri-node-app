f2ac4a3d01b0433e683e583d62a6ae79307b84a4
Cecily <Sanders.Cecily@gmail.com>
2019-01-24 16:15:00 -0500

    finished all functionalities -- had to rewrite into a function for last use case

--- liri.js
+++ liri.js
@@ -1,85 +1,134 @@
 require("dotenv").config();
+//requirements to run the program
 var axios = require("axios");
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var moment = require("moment");
+var fs = require("fs");
 
 //key to search anything
 var command = process.argv[2];
 var ansarry= process.argv;
 ansarry.splice(0,3);
 
-switch (command){
-    case "concert-this":
-        //code to look through bands in town
-        var query = ansarry.join(" ");
-        axios.request("https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp").then(
-            function(response){
-                let date = moment(response.data[0].datetime).format("MM-DD-YYYY");
-                console.log("-------------------------------------------------------------- \n"
-                    + "Name of Venue: " + response.data[0].venue.name + "\n"
-                    + "Venue Location: " + response.data[0].venue.city + "\n"
-                    + "Date of Event: " + date + "\n"
-                    + "--------------------------------------------------------------"
-                );
-            }
-        )
-        break;
+//running the function
+processData(command, ansarry);
 
-    case "spotify-this-song":
-        //code to search spotify
-        var spotify = new Spotify(keys.spotify);
-        var query = ansarry.join(" ");
-        spotify.search({ type: 'track', query: query }, function(err, data) {
-            if (err) {
-              return console.log('Error occurred: ' + err);
-            }
-          console.log(data); 
-          });
-        break;
-
-    case "movie-this":
-        //code to search OMDB
-        var query = ansarry.join("+");
-        //if nothing was entered.
-        if (query === ""){
-            axios.request("http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=trilogy")
-            .then(
-                function(response) {
-                    console.log( "-------------------------------------------------------------- \n"
-                    + "Movie Title: "+ response.data.Title +"\n" 
-                    + "Year Relaeased: " + response.data.Year + "\n"
-                    + "IMBD Rating: " + response.data.imdbRating + "\n"
-                    + "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n"
-                    + "Country Produced: " + response.data.Country + "\n"
-                    + "Language: " + response.data.Language + "\n"
-                    + "Plot of the Movie: " + response.data.Plot + "\n"
-                    + "Actors in the Movie: " + response.data.Actors + "\n"
-                    + "--------------------------------------------------------------"
-                    ); 
+//create the function 
+function processData (command, input){
+    var ansarry = input;
+    switch (command){
+        case "concert-this":
+            //code to look through bands in town
+            var query = ansarry.join(" ");
+            axios.request("https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp").then(
+                function(response){
+                    let date = moment(response.data[0].datetime).format("MM-DD-YYYY");
+                    console.log("-------------------------------------------------------------- \n"
+                        + "Name of Venue: " + response.data[0].venue.name + "\n"
+                        + "Venue Location: " + response.data[0].venue.city + "\n"
+                        + "Date of Event: " + date + "\n"
+                        + "--------------------------------------------------------------"
+                    );
                 }
             )
-        }else{ //if there was any entry
-            axios.request("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy")
-            .then(
-                function(response) {
-                    console.log("-------------------------------------------------------------- \n"
-                    + "Movie Title: "+ response.data.Title +"\n" 
-                    + "Year Relaeased: " + response.data.Year + "\n"
-                    + "IMBD Rating: " + response.data.imdbRating + "\n"
-                    + "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n"
-                    + "Country Produced: " + response.data.Country + "\n"
-                    + "Language: " + response.data.Language + "\n"
-                    + "Plot of the Movie: " + response.data.Plot + "\n"
-                    + "Actors in the Movie: " + response.data.Actors + "\n"
-                    + "--------------------------------------------------------------"
-                    ); 
-                }
-                );
+            break;
+    
+        case "spotify-this-song":
+            //code to search spotify
+            var spotify = new Spotify(keys.spotify);
+            if(Array.isArray(ansarry)){
+                var query = ansarry.join(" ");
+            }else{
+                var query = ansarry;
             }
-        break;
+            //if no entry
+            if (query===""){
+                spotify.search({type:"track", query: "The Sign"}, function(err, data) {
+                    if (err) {
+                      return console.log('Error occurred: ' + err);
+                    }
+                  
+                console.log("-------------------------------------------------------------- \n"
+                        + "Artist Name: " + data.tracks.items[0].album.artists[0].name + "\n"
+                        + "From Abulm: " + data.tracks.items[0].album.name + "\n"
+                        + "Preview Link: " + data.tracks.items[0].href + "\n"
+                        + "--------------------------------------------------------------"
+                    );
+                });
+            }else{ //if song is entered...
+                spotify.search({type:"track", query: query}, function(err, data) {
+                    if (err) {
+                      return console.log('Error occurred: ' + err);
+                    }
+                  
+                console.log("-------------------------------------------------------------- \n"
+                        + "Artist Name: " + data.tracks.items[0].album.artists[0].name + "\n"
+                        + "From Abulm: " + data.tracks.items[0].album.name + "\n"
+                        + "Preview Link: " + data.tracks.items[0].href + "\n"
+                        + "--------------------------------------------------------------"
+                    );
+                });
+            }
+            break;
+    
+        case "movie-this":
+            //code to search OMDB
+            var query = ansarry.join("+");
+            //if nothing was entered.
+            if (query === ""){
+                axios.request("http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=trilogy")
+                .then(
+                    function(response) {
+                        console.log( "-------------------------------------------------------------- \n"
+                        + "Movie Title: "+ response.data.Title +"\n" 
+                        + "Year Relaeased: " + response.data.Year + "\n"
+                        + "IMBD Rating: " + response.data.imdbRating + "\n"
+                        + "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n"
+                        + "Country Produced: " + response.data.Country + "\n"
+                        + "Language: " + response.data.Language + "\n"
+                        + "Plot of the Movie: " + response.data.Plot + "\n"
+                        + "Actors in the Movie: " + response.data.Actors + "\n"
+                        + "--------------------------------------------------------------"
+                        ); 
+                    }
+                )
+            }else{ //if there was any entry
+                axios.request("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy")
+                .then(
+                    function(response) {
+                        console.log("-------------------------------------------------------------- \n"
+                        + "Movie Title: "+ response.data.Title +"\n" 
+                        + "Year Relaeased: " + response.data.Year + "\n"
+                        + "IMBD Rating: " + response.data.imdbRating + "\n"
+                        + "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n"
+                        + "Country Produced: " + response.data.Country + "\n"
+                        + "Language: " + response.data.Language + "\n"
+                        + "Plot of the Movie: " + response.data.Plot + "\n"
+                        + "Actors in the Movie: " + response.data.Actors + "\n"
+                        + "--------------------------------------------------------------"
+                        ); 
+                    }
+                    );
+                }
+            break;
+    
+        case "do-what-it-says":
+            //code to read random.txt
+            fs.readFile("random.txt", "utf-8", function(error, data) {
+                if (error) {
+                  return console.log(error);
+                }
+              
+                console.log(data);
+                var dataArr = data.split(",");
+                processData(dataArr[0], dataArr[1]);
+              
+              });
+            break;
+    }
+}
 
-    case "do-what-it-says":
-        //code to read random.txt
-        break;
+function logOutput (){
+
 }
